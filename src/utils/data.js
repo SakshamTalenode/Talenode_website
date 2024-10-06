c71432ab52695e8196389256e980fbc24f79c1e0
= <=>
2024-10-06 23:40:30 +0530

    images added

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,8 +1,27 @@
 import datadrive from "../assets/DataDrive-hero.png";
 import datadriveimg from "../assets/landing-img.png";
+import valhome from "../assets/IMG6-8.png";
+import valhero from "../assets/IMG9-8.png";
+import valchallenge from "../assets/IMG10-8.png";
+import vala from "../assets/IMG11a-8.png";
+import valb from "../assets/IMG11b-8.png";
+import valc from "../assets/IMG11c-8.png";
+import valwhy from "../assets/IMG12-8.png";
+import sighthome from "../assets/IMG7-8.png";
+import sighthero from "../assets/IMG13-8.png";
+import sightchallenge from "../assets/IMG14-8.png";
+import sighta from "../assets/IMG15-8.png";
+import sightb from "../assets/IMG15b-8.png";
+import sightwhy from "../assets/IMG16-8.png";
+import nudgehome from "../assets/IMG8-8.png";
+import nudgehero from "../assets/IMG17-8.png";
+import nudgechal from "../assets/IMG18_1-8.png";
+import nudgea from "../assets/IMG19-8.png";
+import nudgewhy from "../assets/IMG20-8.png";
 import leader from "../assets/leader.png";
 
 export const products = [
+  // DATA VALIDATOR
   {
     name: "Data Validator",
     id: "one",
@@ -10,13 +29,13 @@ export const products = [
     headerLand: "Data Validator",
     tagLand:
       "Accurate, Complete and Timely (ACT) People Data, All the Time. \nAlways-ready, reliable people data for reporting, compliance and analysis with our intelligent validation tool. No need to spend hours in polishing the raw material you rely on for actioninig.",
-    heroImg: datadrive,
+    heroImg: valhero,
     tagHome: "Accurate, Complete and Timely (ACT) People Data, All the Time.",
-    imgHome: datadriveimg,
+    imgHome: valhome,
     challengeStatement: "The Challenge of Managing People Data",
     challengeDesc:
       "People data is fragmented and becoming exponentially dynamic with every passing moment.  Organizations are investing inordinate efforts in prepping data but  old-school data management is slow and error-prone, leaving them stuck with messy, unreliable data that is far from ready for action. Poor-quality data leads to erroneous insights and conclusions, which impacts leadership decisions and their trust in data.",
-    challengeImg: datadrive,
+    challengeImg: valchallenge,
     header2:
       "Meet Data Validator - Your Solution for Clean, Validated People Data",
     tag2: "Data Validator is your go-to platform for clean, actionable people data. It pulls in data and runs it through smart, pre-built rulesets to instantly flag errors. You can fix issues on the spot, add exceptions, and even create new rules, ensuring your data is always accurate and ready to roll.",
@@ -26,21 +45,21 @@ export const products = [
         toggle: true,
         desc: "Collects data from your HR system seamlessly",
         demoLink: "https://sakshamsehgal.com",
-        img: datadriveimg,
+        img: vala,
       },
       {
         name: "Error Identification & Correction:",
         toggle: false,
         desc: "Highlights errors and guides you through the correction process.",
         demoLink: "",
-        img: datadriveimg,
+        img: valb,
       },
       {
         name: "Downloadable Clean Data for Action",
         toggle: true,
         desc: "After validation, download the updated, error-free data for further reporting, analysis, visualization.",
         demoLink: "",
-        img: datadriveimg,
+        img: valc,
       },
     ],
 
@@ -68,7 +87,7 @@ export const products = [
         "Adapt the tool to your organization realities with custom rulesets",
       ],
     },
-    benefitsImg: datadriveimg,
+    benefitsImg: valwhy,
     demo: "",
     this: "diff",
 
@@ -88,6 +107,7 @@ export const products = [
     toggle: true,
   },
 
+  // DATA SIGHT
   {
     name: "Data Sight",
     id: "two",
@@ -95,13 +115,13 @@ export const products = [
     headerLand: "Data Sight",
     tagLand:
       "Transforming Data into Strategic Advantage. Fuel your organization with curated metrics and insightful dashboards to drive intelligent, data-driven decisions",
-    heroImg: datadrive,
+    heroImg: sighthero,
     tagHome: "Choose from our Metrics Library as per your organization realities. Insightful dashboards that Transform Data into Strategic Advantage.",
-    imgHome: datadriveimg,
+    imgHome: sighthome,
     challengeStatement: "The Challenges of Making Data-Driven Decisions",
     challengeDesc:
       "In today's fast-paced business environment, making the right decision is crucial. The sheer volume of data, coupled with the complexity of choosing the right metrics and making sense of them, can lead to missed opportunities and inefficiencies.",
-    challengeImg: datadrive,
+    challengeImg: sightchallenge,
     header2:
       "Data Sight - Transforming Data into Strategic Advantage",
     tag2: "Data Sight combines the power of expertly curated metrics with intuitive dashboards, ensuring that your organization is always one step ahead. Whether you're refining your measurement strategy or crafting compelling data narratives, Data Sight is your partner in turning data into actionable insights.",
@@ -111,14 +131,14 @@ export const products = [
         toggle: true,
         desc: "Laying a Strong Foundation for Data-Driven Decisions. Effective reporting and analytics start with the right metrics. Our Metrics Library offers a selection of 250+ leading and lagging metrics across various industries and talent functions. We provide expert guidance to help you identify the most relevant metrics based on your industry, decision-making needs, function, and maturity level.",
         demoLink: "/products/DataSight/Metrics Library",
-        img: datadriveimg,
+        img: sighta,
       },
       {
         name: "DashView",
         toggle: false,
         desc: "Once your metrics are defined, we bring them to life through intuitive, narrative-driven dashboards on platforms like Power BI, Tableau, and Qlik. These aren’t your typical dashboards—our solutions are designed to reveal the insights that matter, enabling every member of your organization to take informed, timely actions.",
         demoLink: "/products/DataSight/DashView",
-        img: datadriveimg,
+        img: sightb,
       },
     ],
 
@@ -145,7 +165,7 @@ export const products = [
         "Stay ahead of the curve with dashboards that allow for rapid, intelligent action.",
       ],
     },
-    benefitsImg: datadriveimg,
+    benefitsImg: sightwhy,
     demo: "",
     this: "diff",
 
@@ -163,6 +183,7 @@ export const products = [
     toggle: false,
   },
 
+  // NUDGE
   {
     name: "Nudge",
     id: "three",
@@ -170,13 +191,13 @@ export const products = [
     headerLand: "Nudge",
     tagLand:
       "Stay Ahead with proactive actions. Tired of cluttered dashboard and reactive decisions. Let Nudge keep you ahead of the game by guiding you to take timely actions to address key organization challenges.",
-    heroImg: datadrive,
+    heroImg: nudgehero,
     tagHome: "Stay Ahead of the Curve with Bold, Data-Led Moves. Take actions when you need to",
-    imgHome: datadriveimg,
+    imgHome: nudgehome,
     challengeStatement: "The Problem with Insights and Visualization",
     challengeDesc:
       "Most companies focus on generating metrics and dashboards to garner insights for people decisions. This approach does not necessary work. With limited real estate on dashboards, organizations design them basis what they believe they should monitor, thereby infusing bias in the process. Further, effective dashboard usage relies on the skills of users - can they slice and dice data to unearth insights from self-serving dashboards. Thus, the focus of the entire process shifts from actual problem solving and actioning to insighting. This creates a perception in the minds of leaders that data has limited actionability. ",
-    challengeImg: datadrive,
+    challengeImg: nudgechal,
     header2:
       "Meet Nudge - Your Guide to Make Bold Moves",
     tag2: "Nudge takes a different approach - it maps the journey by clubbing metrics with insights to ascertain what actions to take. Rather than focusing on limited metrics to draw insights, it ingests your entire org data and automatically identifies the metrics you should track. This transforms the 'Real Estate' problem to the 'Real State' solution. You can further customize them and add tripwires as per your operating envrionment and organization realities. No more guessing or waiting for dashboards to update. Nudge alerts the right stakeholders the moment something goes off track, explaining what’s wrong, why it’s happening, and what actions to take. So rather than look for insights in the heap of data, let data guide you to insights actions.",
@@ -186,28 +207,28 @@ export const products = [
         toggle: true,
         desc: "Nudge identifies the key metrics your organization needs to track, removing the guesswork.",
         demoLink: "https://talenode.com",
-        img: datadriveimg,
+        img: nudgea,
       },
       {
         name: "Continous Monitoring",
         toggle: false,
         desc: "Nudge continuously monitors these metrics against your predefined thresholds/tripwires.",
         demoLink: "https://talenode.com",
-        img: datadriveimg,
+        img: nudgea,
       },
       {
         name: "Proactive Alerts",
         toggle: true,
         desc: "Receive instant notifications when metrics deviate from the norm, with clear explanations and action steps.",
         demoLink: "",
-        img: datadriveimg,
+        img: nudgea,
       },
       {
         name: "Customizable Thresholds",
         toggle: false,
         desc: "Set your own thresholds and metrics, so you’re always in control of what matters most.",
         demoLink: "",
-        img: datadriveimg,
+        img: nudgea,
       },
     ],
 
@@ -234,7 +255,7 @@ export const products = [
         "Adapt Nudge to your organization’s unique needs with customizable thresholds and metrics.",
       ],
     },
-    benefitsImg: datadriveimg,
+    benefitsImg: nudgewhy,
     demo: "",
     this: "diff",
 
